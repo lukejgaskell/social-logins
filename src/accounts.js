317a6d471e6820ba25aeb067113202651c50d7fc
Luke Gaskell <ljgaskell@liberty.edu>
2017-10-02 18:49:12 -0400

    removed clustering

--- src/accounts.js
+++ src/accounts.js
@@ -11,35 +11,17 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require("./routes");
 
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
 const PORT = 8000;
 const HOST = '0.0.0.0';
 
-if (cluster.isMaster) {
-    console.log(`Master ${process.pid} is running`);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+passportConfig.configure(app);
 
-    // Fork workers.
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
-
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`worker ${worker.process.pid} died`);
-    });
-} else {
-
-    app.use(cookieParser());
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
-    passportConfig.configure(app);
-
-    routes.createRoutes(app);
+routes.createRoutes(app);
 
 
-    app.listen(PORT, HOST, () => {
-        console.log(`listening on http://${HOST}:${PORT}!`);
-    });
-
-    console.log(`Worker ${process.pid} started`);
-}
+app.listen(PORT, HOST, () => {
+    console.log(`listening on http://${HOST}:${PORT}!`);
+});
