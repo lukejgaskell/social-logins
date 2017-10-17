45129d9b5ee02fafde56372e2f07daf0650d4747
Luke Gaskell <ljgaskell@liberty.edu>
2017-10-16 21:24:00 -0400

    updated properties

--- src/accounts.js
+++ src/accounts.js
@@ -21,20 +21,7 @@ var controllers = readDirectory('./src');
 controllers.forEach(controllerFile => {
     require('../' + controllerFile)(app);
 });
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-app.use(allowCrossDomain);
 app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json());
